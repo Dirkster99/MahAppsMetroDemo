63888f0922f60fe8a3062ceb0aada8e2f2758257
Dirkster99 <Dirk.Bahle@gmx.de>
2019-08-26 22:07:18 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+
+# Overview
+
+This repository contains a simple demo application that shows how MahApps.Metro can be used to implement a similar
+main window as in VS 2019 (with menu bar in title bar section).
+
+![](help/Untitled.png)
+
+![](help/Untitled1.png)
---
+++ source/MahAppsMetroDemo/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MahAppsMetroDemo
+{
+    using MahApps.Metro.Controls;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
